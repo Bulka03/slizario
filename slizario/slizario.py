3361a440b75750ccd9159bf834bd981186ca738d
Bulka03 <118056802+Bulka03@users.noreply.github.com>
2025-05-03 20:03:08 +0500

    Add files via upload

--- slizario/slizario.py
+++ slizario/slizario.py
@@ -30,43 +30,54 @@ class Player(GameSprite):
         self.rect.x += self.speed_x
         
 class Tail(GameSprite):
-    def __init__(self, player_image, player_x, player_y, player_width, player_height, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_width, player_height, player_speed, timer):
         super().__init__(player_image, player_x, player_y, player_width, player_height, player_speed)
         self.speed_x = self.speed
         self.speed_y = 0
+        self.timer = timer
 
-main_player = Player("New Piskel (2).png", 300, 400, 70, 70, 30)
+    def update(self):
+        print(self.timer)
+        self.timer -= 1
+        if self.timer <= 0:
+            self.image = transform.scale(image.load("New Piskel.png"), (69, 69))
+            self.kill()
+
+
+
+
+main_player = Player("New Piskel (2).png", 300, 400, 30, 30, 30)
  
-prize = GameSprite("New Piskel (1).png", randint(80, 635), 100, 100, 100, randint(16, 23)*30)
+prize = GameSprite("New Piskel (1).png", randint(80, 635), 100, 30, 30, randint(16, 23)*30)
 
 tails = sprite.Group()
-
-
 
 
 game = True
 finish = False
 
 
-score = 0
+score = 1
 
 wait = 0
 while game:
     if finish != True:
-        
         if wait == 0:
             window.blit(background,(0, 0))
             main_player.update()
             main_player.reset()
             prize.update()
             prize.reset()
-            tail.update()
-            tail = ("New Piskel", player.rect.x-player.speed_x, player.rect.y-player.speed_y)
-                tails.add(tail)
-            if player.colliderect(prize):
+            tail = Tail("New Piskel.png", main_player.rect.x - main_player.speed_x, main_player.rect.y - main_player.speed_y, 69, 69, 30, score)
+            tails.add(tail)
+            tails.update()
+            tails.draw(window)
+            if main_player.colliderect(prize):
                 score += 1
                 prize.rect.x = randint(0, 23)*30
-                prize.rect.y = randint(0, 16)*16
+                prize.rect.y = randint(0, 16)*30
+            if sprite.spritecollide(main_player, tails, True):
+                finish = True
             wait = 30   
         wait -= 1
     keys = key.get_pressed()
